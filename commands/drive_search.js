efb5a187109a4d3dbe6479000e5a2b3e0ea2fa36
Sidharth Shambu <49567934+shambu2k@users.noreply.github.com>
2021-03-23 03:04:35 +0530

    refactor: remove console.log

--- commands/drive_search.js
+++ commands/drive_search.js
@@ -6,7 +6,6 @@ module.exports = {
 	description: 'Access the drive files\nUsage - !psiva search <one-word-search-query-here>',
 	async execute(message, args) {
 		try {
-			console.log('searching ' + args[1]);
 			let data = await gdrive.searchFolder(args[1]);
 			const embedMsg = new Discord.MessageEmbed()
 				.setTitle(`Here are your search results for - ${args[1]}`)
